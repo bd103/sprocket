5970a26fb3d80e3910321b650647560f809d2be5
BD103 <59022059+BD103@users.noreply.github.com>
2026-06-10 20:27:16 -0400

    fix(py): importing items from submodules
    `import sprocket_py` was working, but `from sprocket_py.diagnostics import Mode` was not. Turns out I was running into <https://github.com/PyO3/pyo3/issues/759>, so I adapted the solution in <https://github.com/PyO3/pyo3/issues/759#issuecomment-4042683948> to manually add the submodules to `sys.modules`.

--- crates/sprocket-py/src/lib.rs
+++ crates/sprocket-py/src/lib.rs
@@ -10,4 +10,50 @@ use pyo3::prelude::*;
 /// Python bindings to [Sprocket](https://sprocket.bio), a bioinformatics toolkit for Workflow
 /// Description Language (WDL).
 #[pymodule]
-mod sprocket_bio {}
+mod sprocket_bio {
+    use pyo3::prelude::*;
+    use pyo3::types::PyString;
+
+    /// Initializes the module.
+    #[pymodule_init]
+    fn init(module: &Bound<'_, PyModule>) -> PyResult<()> {
+        /// Recursively registers all submodules to
+        /// [`sys.modules`](https://docs.python.org/3.9/library/sys.html#sys.modules).
+        ///
+        /// This is required to support importing items directly from submodules
+        /// (ex. `from sprocket_bio.diagnostics import Mode`). For more
+        /// information, see [pyo3#759](https://github.com/PyO3/pyo3/issues/759).
+        fn register_submodules(
+            module: &Bound<'_, PyModule>,
+            parent_name: &str,
+            sys_modules: &Bound<'_, PyAny>,
+        ) -> PyResult<()> {
+            // Loop through the names of all items in the module.
+            for item_name in module.index()? {
+                // Cast from `PyAny` to `PyString`.
+                let item_name: &Bound<'_, PyString> = item_name.cast()?;
+                // Get the item from its name.
+                let item = module.getattr(item_name)?;
+
+                // If the item is a submodule...
+                if let Ok(submodule) = item.cast::<PyModule>() {
+                    let submodule_name = format!("{parent_name}.{item_name}");
+
+                    // ...add the submodule to `sys.modules`.
+                    sys_modules.set_item(&submodule_name, submodule)?;
+
+                    register_submodules(submodule, &submodule_name, sys_modules)?;
+                }
+            }
+
+            Ok(())
+        }
+
+        register_submodules(
+            module,
+            "sprocket_bio",
+            // Get the `sys.modules` dictionary.
+            &module.py().import("sys")?.getattr("modules")?,
+        )
+    }
+}
